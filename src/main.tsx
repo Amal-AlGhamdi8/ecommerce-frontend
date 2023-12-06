50267cfb9671e2692423a39d0b8bc01d30674ad6
Amal Gh <Algamdi_8@hotmail.com>
2023-12-06 21:13:39 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
 import { store } from './redux/store'
-import App from './App'
+import App from '../App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
