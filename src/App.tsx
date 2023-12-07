291ea1a7534a76f2529c5f58268a18ad243df478
Amal Gh <Algamdi_8@hotmail.com>
2023-12-07 15:10:39 +0300

    fix

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import '../src/App.css'
+import '../src/styles/home.css' ;
+import '../src/styles/style.css' ;
+import Index from './routes/Routers';
+
+function App() {
+  return (
+    <div className="App">
+      <Index />
+    </div>
+  )
+}
+
+export default App
